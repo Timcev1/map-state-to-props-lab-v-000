12c60a2ce53f63ab7d385a246982a0ec25742e81
Timothy Cevallos <timcev@gmail.com>
2017-10-18 18:09:31 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import UserInput from './components/UserInput'
+import {ConnectedUsers} from './components/Users'
 
 export class App extends Component {
   render() {
